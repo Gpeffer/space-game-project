7c8cb90e74ad7232b4f0a8932d7d413ad86bbd7d
Sam Segars <sam.segars@yahoo.com>
2021-11-09 15:44:45 -0500

    Added base modules for the foundation of the project

---
+++ game-files/main.py
@@ -0,0 +1,17 @@
+import ship 
+import planets
+import userinfo
+
+
+def Main_loop():
+    answer = 0
+    while answer != "q":
+        print("Units:")
+        print("Location:")
+        print("")    
+
+
+
+
+Main_loop()
+Print("Game Ended")
